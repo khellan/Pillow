618a4fe7a006dc358ac54e8cf1dabec2bafd485b
khellan <kyosha@gmail.com>
2010-01-29 15:39:11 +0100

    Implemented the correct key-based reduction and also extended reducer lookup to generic.

--- src/pillow_reducer.erl
+++ src/pillow_reducer.erl
@@ -7,31 +7,35 @@ content_types_provided(ReqData, Context) ->
     
 init([]) -> {ok, undefined}.
 
-get_value(Tuple) ->
+add_tuple(Tuple, Acc) ->
     case Tuple of
         {struct, Pair} ->
+            {value, {<<"key">>, Key}} = lists:keysearch(<<"key">>, 1, Pair),
             {value, {<<"value">>, Value}} = lists:keysearch(<<"value">>, 1, Pair),
-            Value;
-        false -> error
+            case dict:find(Key, Acc) of
+                {ok, OldValue} -> dict:store(Key, OldValue + Value, Acc);
+                error -> dict:store(Key, Value, Acc)
+            end;
+        _Other -> error
     end.
 
 sum_reducer(List) ->
-    lists:sum(lists:map(fun(Tuple) -> get_value(Tuple) end, List)).
+    Result = dict:to_list(lists:foldl(fun(Tuple, Acc) -> add_tuple(Tuple, Acc) end, dict:new(), List)),
+    lists:map(fun(Tuple) -> {struct, [{<<"key">>, element(1, Tuple)}, {<<"value">>, element(2, Tuple)}]} end, Result).
 
 get_reducers() ->
-    Dict1 = dict:store("userprofiles", "reports", "clicked_recommendations_vendor_timestamps", fun(List) -> sum_reducer(List) end, dict:new()),
-    Dict2 = dict:store("userprofiles", "reports", "clicked_recommendations_vendor_totals", fun(List) -> sum_reducer(List) end, Dict1),
-    Dict3 = dict:store("userprofiles", "reports", "purchase_count", fun(List) -> sum_reducer(List) end, Dict2),
-    Dict4 = dict:store("userprofiles", "reports", "recommendations_vendor_timestamps", fun(List) -> sum_reducer(List) end, Dict3),
-    Dict5 = dict:store("userprofiles", "reports", "recommendations_vendor_totals", fun(List) -> sum_reducer(List) end, Dict4),
-    Dict6 = dict:store("userprofiles", "reports", "vendor_timestamps", fun(List) -> sum_reducer(List) end, Dict5),
-    Dict7 = dict:store("userprofiles", "reports", "vendor_totals", fun(List) -> sum_reducer(List) end, Dict6),
-    Dict8 = dict:store("userprofiles", "reports", "vendor_uniques", fun(List) -> sum_reducer(List) end, Dict7),
+    Dict1 = dict:store({"userprofiles", "reports", "clicked_recommendations_vendor_timestamps"}, fun(List) -> sum_reducer(List) end, dict:new()),
+    Dict2 = dict:store({"userprofiles", "reports", "clicked_recommendations_vendor_totals"}, fun(List) -> sum_reducer(List) end, Dict1),
+    Dict3 = dict:store({"userprofiles", "reports", "purchase_count"}, fun(List) -> sum_reducer(List) end, Dict2),
+    Dict4 = dict:store({"userprofiles", "reports", "recommendations_vendor_timestamps"}, fun(List) -> sum_reducer(List) end, Dict3),
+    Dict5 = dict:store({"userprofiles", "reports", "recommendations_vendor_totals"}, fun(List) -> sum_reducer(List) end, Dict4),
+    Dict6 = dict:store({"userprofiles", "reports", "vendor_timestamps"}, fun(List) -> sum_reducer(List) end, Dict5),
+    Dict7 = dict:store({"userprofiles", "reports", "vendor_totals"}, fun(List) -> sum_reducer(List) end, Dict6),
+    Dict8 = dict:store({"userprofiles", "reports", "vendor_uniques"}, fun(List) -> sum_reducer(List) end, Dict7),
     Dict8.
 
-get_reducer(Db, _Design, View) ->
-    case Db
-    case dict:find(View, get_reducers()) of
+get_reducer(Db, Design, View) ->
+    case dict:find({Db, Design, View}, get_reducers()) of
         {ok, Reducer} ->Reducer;
         error -> fun(List) -> List end
     end.
@@ -77,12 +81,13 @@ reduce_result(Db, Design, View, RawResult) ->
 
 get_db_design_view(ReqData) ->
     PathElements = wrq:path_tokens(ReqData),
-    {lists:nth(2, PathElements), lists:nth(3, PathElements), lists:nth(5, PathElements)}
+    {lists:nth(1, PathElements), lists:nth(3, PathElements), lists:nth(5, PathElements)}.
 
 get_all_server_results(ReqData) ->
     Servers = lists:map(fun({_, Server}) -> single_server_result_retriever(Server, ReqData) end, pillow_routing_table:to_list(pillow_routing_table:init())),
     {Db, Design, View} = get_db_design_view(ReqData),
     reduce_result(Db, Design, View, get_all_responses(Servers)).
+%    io_lib:format("Db: ~s, Design: ~s, View: ~s", [Db, Design, View]).
 
 to_json(ReqData, Context) ->
     Results = mochijson2:encode(get_all_server_results(ReqData)),
