56e28b650593a345b89f32c9fe249797e4a5ef0a
khellan <kyosha@gmail.com>
2010-02-03 16:59:10 +0100

    Added a function for upgrading the pillow routing table.

--- src/pillow.erl
+++ src/pillow.erl
@@ -1,5 +1,5 @@
 -module(pillow).
--export([start/0, stop/0]).
+-export([start/0, stop/0, update_routing_table/0]).
 
 ensure_started(App) ->
     case application:start(App) of
@@ -25,3 +25,18 @@ stop() ->
     application:stop(crypto),
     application:stop(ibrowse),
     Res.
+
+%%--------------------------------------------------------------------
+%% Function: update_routing_table/0
+%% Description: Retrieves result from a single server
+%% Returns: The result retrieved from the server
+%%--------------------------------------------------------------------
+update_routing_table() ->
+    [{attributes, PreAttributes}] = lists:filter(fun(X) -> element(1, X) == attributes end, pillow_routing_table:module_info()),
+    [{vsn, [PreVersion]}] = lists:filter(fun(X) -> element(1, X) == vsn end, PreAttributes),
+    code:purge(pillow_routing_table),
+    code:load_file(pillow_routing_table),
+    [{attributes, PostAttributes}] = lists:filter(fun(X) -> element(1, X) == attributes end, pillow_routing_table:module_info()),
+    [{vsn, [PostVersion]}] = lists:filter(fun(X) -> element(1, X) == vsn end, PostAttributes),
+    {upgrade, PreVersion, PostVersion}.
+
